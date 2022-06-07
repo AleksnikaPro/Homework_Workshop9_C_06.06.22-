7515a7329ced869ed0c6f1fe38bdb38617149430
Aleksandra Popova <alexnika@bk.ru>
2022-06-07 14:40:35 +0300

    added task_1

---
+++ task_1/Program.cs
@@ -0,0 +1,9 @@
+﻿// 1. Показать натуральные числа от M до N, N и M заданы.
+
+void ShowNumber(int m, int n)
+{
+    if (n < m) return;
+    ShowNumber(m, n - 1);
+    Console.Write(n + " ");
+}
+ShowNumber(20, 35);
