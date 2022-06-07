137f58f53f3583ed4c97e6a48975515691d3b5a0
Aleksandra Popova <alexnika@bk.ru>
2022-06-07 14:40:56 +0300

    added task_2

---
+++ task_2/Program.cs
@@ -0,0 +1,8 @@
+﻿// 2. Найти сумму элементов от M до N, N и M заданы.
+
+int SumOfNumbers(int m, int n)
+{
+    if (n == m) return n;
+    return m + SumOfNumbers(m + 1, n);
+}
+Console.WriteLine(SumOfNumbers(1, 10));
